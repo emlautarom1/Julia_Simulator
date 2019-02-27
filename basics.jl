3a4a926dbf69f7fa4c5c0bcf9a2a540faa821ea7
lautaroem1 <emlautarom1@gmail.com>
2019-02-27 19:25:18 -0300

    Formatting and better check for emptys

--- basics.jl
+++ basics.jl
@@ -1,8 +1,8 @@
 module basics
 
-export +,radix,byte,radixcompi,radixcompr,digitcompi,digitcompr,magni,magnr,signmagni,signmagnr
-export biasi,biasr,hidebit,insertbit,truncate,round,trueround,normalize,flbsi,flbsr
-export inPattern,decode
+export +, radix, byte, radixcompi, radixcompr, digitcompi, digitcompr, magni, magnr, signmagni, signmagnr
+export biasi, biasr, hidebit, insertbit, truncate, round, trueround, normalize, flbsi, flbsr
+export inPattern, decode
 
 import Base.+
 +(of::Int64, r::UnitRange{Int64})::UnitRange{Int64} = UnitRange(r[1] + of, r[end] + of)
@@ -26,17 +26,19 @@ const expextr = radix^(length(Exp) - 1)
 #-----------------------------------------------------------------
 
 function radixcompi(rep::Vector{Int8})::BigInt
-    local t::BigInt = 0;
-    try t = rep[1] catch; return 0 end
-    local modulus::BigInt = (convert(BigInt, radix))^length(rep)
-    for i in 2:(axes(rep, 1)[end])
-        t = t * radix + rep[i]
+    if (isempty(rep))
+        return 0
+    else
+        local t::BigInt = 0
+        local modulus::BigInt = (BigInt(radix))^length(rep)
+        for i in 2:(axes(rep, 1)[end])
+            t = t * radix + rep[i]
+        end
+        if t >= (modulus >> 1)
+            t = t - modulus
+        end    
+        return t
     end
-    if t >= (modulus >> 1)
-        t = t - modulus
-    end
-    
-    return t
 end
 
 function radixcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
@@ -44,7 +46,7 @@ function radixcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
     local xmax = 0
 
     t::Array{Int8} = zeros(Int8, size)
-    local extr::BigInt = div((convert(BigInt, radix))^size, 2)
+    local extr::BigInt = div(BigInt(radix)^size, 2)
     if num >= extr
         xmax = 1
     elseif num < -extr
@@ -59,7 +61,7 @@ function radixcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
 end
 
 function radixcompr(size::Int64, num::Int64)::Tuple{Vector{Int8},Int64,Int64}
-    local t::BigInt = convert(BigInt, num)
+    local t::BigInt = BigInt(num)
     return radixcompr(size, t)
 end
 
@@ -68,17 +70,19 @@ end
 #---------------------------------------------------------
 
 function digitcompi(rep::Array{Int8})::BigInt
-    local t::BigInt = 0
-    try t = rep[1] catch; return 0 end
-    local modulus::BigInt = (convert(BigInt, radix))^length(rep) - 1
-    for i in 2:axes(rep, 1)[end]
-        t = t * radix + rep[i]
+    if (isempty(rep))
+        return 0
+    else
+        local t::BigInt = 0
+        local modulus::BigInt = BigInt(radix)^length(rep) - 1
+        for i in 2:axes(rep, 1)[end]
+            t = t * radix + rep[i]
+        end
+        if t >= modulus / 2
+            t = t - modulus
+        end
+        return t
     end
-    if t >= modulus / 2
-        t = t - modulus
-    end
-    
-    return t
 end
 
 function digitcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
@@ -86,7 +90,7 @@ function digitcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
     local xmax = 0
 
     t::Vector{Int8} = zeros(Int8, size)
-    local extr::BigInt = (convert(BigInt, radix))^(size - 1)
+    local extr::BigInt = (BigInt(radix))^(size - 1)
     local modulus::BigInt = extr * radix - 1
     if num >= extr
         xmax = 1
@@ -103,8 +107,7 @@ function digitcompr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
 end
 
 function digitcompr(size::Int64, num::Int64)::Tuple{Vector{Int8},Int64,Int64}
-    local t::BigInt = convert(BigInt, num)
-    
+    local t::BigInt = BigInt(radix)
     return digitcompr(size, t)
 end
 
@@ -113,13 +116,15 @@ end
 #-----------------------------------------------------------------
 
 function magni(rep::Vector{Int8})::BigInt
-    local t::BigInt = 0
-    try t = rep[1] catch; return 0 end
-    for i in 2:axes(rep, 1)[end]
-        t = (t * radix) + rep[i]
+    if (isempty(rep))
+        return 0
+    else
+        local t::BigInt = 0
+        for i in 2:axes(rep, 1)[end]
+            t = (t * radix) + rep[i]
+        end
+        return t
     end
-    
-    return t
 end
 
 function magnr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
@@ -127,7 +132,7 @@ function magnr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
     local xmax = 0
 
     t::Array{Int8} = zeros(Int8, size)
-    local extr::BigInt = (convert(BigInt, radix))^size
+    local extr::BigInt = (BigInt(radix))^size
     if num >= extr
         xmax = 1
     elseif num < 0
@@ -142,7 +147,7 @@ function magnr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
 end
 
 function magnr(size::Int64, num::Int64)::Tuple{Vector{Int8},Int64,Int64}
-    local t::BigInt = convert(BigInt, num)
+    local t::BigInt = BigInt(radix)
     return magnr(size, t)
 end
 
@@ -152,7 +157,7 @@ end
 
 function signmagni(rep::Vector{Int8})::BigInt
     if (isempty(rep))
-        0
+        return 0
     else
         local t::BigInt = 0
         local modulus::BigInt = radix^(length(rep) - 1)
@@ -166,16 +171,15 @@ end
 function signmagnr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
     local xmin = 0
     local xmax = 0
-    local sign
 
     t::Array{Int8} = zeros(Int8, size)
-    local extr::BigInt = (convert(BigInt, radix))^(size - 1)
+    local extr::BigInt = (BigInt(radix))^(size - 1)
     if num >= extr
         xmax = 1
     elseif num <= -extr
         xmin = 1
     end
-    sign = (num < 0) ? minus : plus
+    local sign = (num < 0) ? minus : plus
     num = abs(num)
     for i in axes(t, 1)
         t[axes(t, 1)[end] - i + 1] = mod(num, radix)
@@ -187,7 +191,7 @@ function signmagnr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
 end
 
 function signmagnr(size::Int64, num::Int64)::Tuple{Vector{Int8},Int64,Int64}
-    local t::BigInt = convert(BigInt, num)
+    local t::BigInt = BigInt(radix)
     return signmagnr(size, t)
 end
 
@@ -212,7 +216,7 @@ function biasr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
     local sign
 
     t::Array{Int8} = zeros(Int8, size)
-    local bias::BigInt = div((convert(BigInt, radix))^size, 2)
+    local bias::BigInt = div((BigInt(radix))^size, 2)
     if num >= bias
         xmax = 1
     elseif num < -bias
@@ -228,7 +232,7 @@ function biasr(size::Int64, num::BigInt)::Tuple{Vector{Int8},Int64,Int64}
 end
 
 function biasr(size::Int64, num::Int64)::Tuple{Vector{Int8},Int64,Int64}
-    local t::BigInt = convert(BigInt, num)
+    local t::BigInt = BigInt(radix)
     biasr(size, t)
 end
 
@@ -326,7 +330,10 @@ end
 #--                   "OpCode"=>("11",1:3),                                     --
 #--                   "Dest"=>("10",10:15))                                     --
 #---------------------------------------------------------------------------------
-function inPattern(form::Array{Int8,3}, patt::String, dict::Dict{String,Tuple{String,UnitRange{Int64}}})::Array{Int8,3}
+function inPattern(form::Array{Int8,3},
+                   patt::String,
+                   dict::Dict{String,Tuple{String,UnitRange{Int64}}})::Array{Int8,3}
+
     local s = size(form)
     local p = replace(replace(patt, r"(0|1)( |$)" => x->"0" * rstrip(x)), r"(?<tok>[a-z][a-z0-9]+( |$))"i => 
                     x->(string(dict[rstrip(x)][1]))^length(dict[rstrip(x)][2]))
@@ -363,20 +370,18 @@ function decode(inst::Array{Int8,1},
     # 00 = 0
     local f = form[:, collect(1:length(inst)),:]
  
-    let i = size(form)[1] # last match index
-        let ty = -1
-            while (ty < 0)
+    local i = size(form)[1] # last match index
+    local ty = -1
+    while (ty < 0)
             # Check reduce: should reduce in only one dimension
-                if reduce(&, (map(|, f[i,:,1], f[i,:,2]) .>= inst)) && reduce(&, (map(<, f[i,:,1], f[i,:,2]) .<= inst))
-                    ty = i
-                else
-                    i -= 1
-                end
-            end
-            local off = Int64(magni(inst[findall(x->x == 1, map(&, f[i,:,1], f[i,:,2]))]))
-            return  oplist[orop[ty] + off]
+        if reduce(&, (map(|, f[i,:,1], f[i,:,2]) .>= inst)) && reduce(&, (map(<, f[i,:,1], f[i,:,2]) .<= inst))
+            ty = i
+        else
+            i -= 1
         end
     end
+    local off = Int64(magni(inst[findall(x->x == 1, map(&, f[i,:,1], f[i,:,2]))]))
+    return oplist[orop[ty] + off]
 end
 
 end #module
